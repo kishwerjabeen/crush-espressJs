df584494cfc69ec2f8eb6b4a9c6537604e9cdddd
Kishwar Jabeen <43556329+kishwerjabeen@users.noreply.github.com>
2020-12-15 10:59:03 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.port | 4000;
+
+// create get funtion 
+
+app.get('/', (req,res)=>{
+  
+    res.setEn
+
+});
+
+
+app.listen(port, () => {
+console.log(`port start : ${port}`);
+});
+
